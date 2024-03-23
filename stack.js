acd6101c918b9cc17c4061f6a9f89c3e774bd6c3
Dustin <dustincheyney@gmail.com>
2024-03-23 13:55:31 -0700

    Completed classes, all tests passed.

--- stack.js
+++ stack.js
@@ -20,26 +20,45 @@ class Stack {
   /** push(val): add new value to end of the stack. Returns undefined. */
 
   push(val) {
+    let newNode = new Node(val);
 
+    if(!this.first){
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let curFirst = this.first;
+      this.first = newNode;
+      this.first.next = curFirst;
+    }
+    this.size ++;
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
-
+    if(!this.first){
+      throw new Error("Stack empty, unable to pop empty Stack!")
+    }
+    let rem = this.first;
+    if(this.first == this.last){
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size --;
+    return rem.val;
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
-
+    return this,this.first.val;
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0;
   }
 }
 
